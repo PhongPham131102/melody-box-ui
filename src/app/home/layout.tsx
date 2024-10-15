d799b22944e565c171f146b95c4f472ec8fb4772
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-15 15:37:56 +0700

    feat (layout): add layout for home page

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,9 @@
+import Header from "@/src/components/header";
+
+export default function HomeLayout() {
+  return (
+    <div className="bg-main w-screen h-screen">
+      <Header />
+    </div>
+  );
+}

d9d65a1b065abefb381be38d00074261807fb015
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-15 16:00:44 +0700

    refactor (components): refactor header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logoApp from "@/public/logo.png";
 import registerIcon from "@/public/icons/register.png";
+
 export default function Header() {
   return (
     <div className="w-full py-2 px-6 bg-header flex justify-between items-center">
@@ -13,10 +14,12 @@ export default function Header() {
           className="ml-5"
         />
       </div>
-      <div className="flex flex-row justify-center items-center gap-1 bg-secondary py-1 px-1.5 rounded-full">
+
+      <div className="flex flex-row justify-center items-center gap-1 py-1 px-1.5 rounded-full bg-secondary group hover:bg-transparent transition-all duration-700">
         <button className="bg-main-text hover:cursor-pointer transition-all duration-300 px-8 py-2 rounded-full text-sm text-white font-bold">
           Đăng Nhập
         </button>
+
         <button className="group flex justify-center items-center gap-1 px-8 py-2 hover:cursor-pointer hover:rounded-full transition-all duration-500 hover:bg-thirst-text text-white-1 text-sm font-bold">
           <div className="transition-all duration-500 brightness-0 filter invert group-hover:brightness-100">
             <Image
@@ -26,7 +29,6 @@ export default function Header() {
               height={14}
             />
           </div>
-
           <p className="transition-all duration-500 group-hover:text-white">
             Đăng Ký
           </p>

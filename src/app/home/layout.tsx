d4e28b3ea8635b9c175a435197bbba107ffec23f
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-17 10:19:48 +0700

    feat (layout): update layout for home page

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,10 +1,21 @@
+/* eslint-disable react/no-children-prop */
 import Header from "@/src/components/header";
+import PlayerController from "@/src/components/player-controller";
+import SideBar from "@/src/components/sidebar";
+import StoreProvider from "../redux.provider";
 
 export default function HomeLayout() {
   return (
-    <div className="bg-main w-full">
+    <div className="bg-main w-full min-h-[100vh] select-none">
       <Header />
-      <div className="w-full bg-main h-[200vh]"></div>
+      <StoreProvider
+        children={
+          <>
+            <SideBar />
+            <PlayerController />
+          </>
+        }
+      />
     </div>
   );
 }

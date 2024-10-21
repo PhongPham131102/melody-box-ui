061c8f07d2b2f5f15bd8984bcfd41ca219c57071
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-21 13:06:25 +0700

    feat (components): add props for playlists components

--- src/components/PlayLists/index.tsx
+++ src/components/PlayLists/index.tsx
@@ -1,22 +1,35 @@
-import PlayList from "../Playlist";
-
-export default function PlayLists() {
+import PlayList, { PlayListProps } from "../Playlist";
+type PlayLists = {
+  title: string;
+  linkUrl?: string;
+  textUrl?: string;
+  playlists: PlayListProps[];
+};
+export default function PlayLists({
+  linkUrl,
+  textUrl,
+  title,
+  playlists,
+}: PlayLists) {
   return (
     <div className="w-full flex flex-col  gap-2 px-3 py-4">
       <div className="w-full flex justify-between items-center py-2">
-        <span className="text-white font-bold text-lg">
-          Nhạc Hot Thịnh Hành
-        </span>
-        {/* <button className="group text-white text-xs bg-main-text hover:brightness-90 py-1 px-3 rounded-full flex gap-1 uppercase">
-          Làm mới
-        </button> */}
+        <span className="text-white font-bold text-lg">{title}</span>
+        {linkUrl && (
+          <button className="group text-white text-xs bg-main-text hover:brightness-90 py-1 px-3 rounded-full flex gap-1 uppercase">
+            {textUrl}
+          </button>
+        )}
       </div>
       <div className="w-full grid grid-cols-5 gap-4">
-        <PlayList />
-        <PlayList />
-        <PlayList />
-        <PlayList />
-        <PlayList />
+        {playlists.map((playlist, index) => (
+          <PlayList
+            key={index}
+            imgUrl={playlist.imgUrl}
+            linkUrl={playlist.linkUrl}
+            title={playlist.title}
+          />
+        ))}
       </div>
     </div>
   );

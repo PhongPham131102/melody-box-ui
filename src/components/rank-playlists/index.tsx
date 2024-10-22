483872b327550ca3e5a01f6e83269f2285a1b39d
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-22 09:45:41 +0700

    refactor: integrate Embla Carousel with RankPlayLists component
    - Replaced static grid-based layout with dynamic carousel using Embla Carousel.
    - Implemented autoplay functionality with configurable options for better user interaction.
    - Updated the RankPlayLists component to display a list of ranked songs using the new carousel system.
    - Integrated `RankItem` component within the carousel for song ranking display.

--- src/components/rank-playlists/index.tsx
+++ src/components/rank-playlists/index.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
-import playIcon from "@/public/icons/play.png";
+"use client";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import RankItem from "../rank-item";
 type RankPlayListProps = {
   title: string;
   linkUrl?: string;
@@ -10,6 +18,78 @@ export default function RankPlayLists({
   textUrl,
   title,
 }: RankPlayListProps) {
+  const arrSongs = [
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "1",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "2",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "3",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "4",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "5",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "6",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "7",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "8",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "9",
+      title: "Trăm ngàn lần sai",
+    },
+    {
+      artist: "Kaylin Trần",
+      date: "18.10.2024",
+      img: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/d/9/a/f/d9af676be118849c492c049b25b78103.jpg",
+      rank: "10",
+      title: "Trăm ngàn lần sai",
+    },
+  ];
   return (
     <div className="w-full flex flex-col  gap-2 px-3 py-4">
       <div className="w-full flex justify-between items-center py-2">
@@ -20,43 +100,46 @@ export default function RankPlayLists({
           </button>
         )}
       </div>
-      <div className="w-full grid grid-cols-3 gap-4">
-        <div className="w-full p-3 bg-line rounded flex gap-3">
-          <div className="hover-zoom group cursor-pointer w-32 aspect-square relative rounded overflow-hidden">
-            <Image
-              src={
-                "https://photo-resize-zmp3.zmdcdn.me/w165_r1x1_jpeg/cover/7/b/e/3/7be33b2123aee6e0982a5fde3c986df6.jpg"
-              }
-              fill
-              alt="Album Cover"
-              className="zoomable rounded group-hover:brightness-75 object-cover"
-            />
-            <Image
-              src={playIcon}
-              alt="Play Icon"
-              layout="fixed"
-              width={12}
-              height={12}
-              className="hidden w-3 h-3 group-hover:absolute group-hover:!block z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 filter invert brightness-0 contrast-200 saturate-150"
-            />
-          </div>
-          <div className="flex h-full w-full flex-col justify-between">
-            <div className="w-full flex flex-col justify-start items-start gap-1">
-              <h4 className="text-white font-semibold text-base">
-                Đừng kéo đôi chân em lại
-              </h4>
-              <p className="text-gray-400 text-sm font-medium">Hà Nhi</p>
-            </div>
-            <div className="w-full flex flex-row justify-between items-end gap-1">
-              <h1 className="opacity-40 text-[40px] font-black text-transparent leading-none text-stroke-2 tracking-widest font-sans">
-                #1
-              </h1>
-              <p className="text-white text-sm font-medium opacity-70">
-                18.10.2024
-              </p>
-            </div>
-          </div>
-        </div>
+      <div className="w-full">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 3000,
+              stopOnFocusIn: true,
+              stopOnMouseEnter: true,
+              stopOnInteraction: true,
+            }),
+          ]}
+          opts={{
+            align: "start",
+            startIndex: 0,
+            duration: 10,
+          }}
+          className="w-full group/outer px-2"
+        >
+          <CarouselContent>
+            {arrSongs.map((song, index) => (
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3" key={index}>
+                <RankItem
+                  artist={song.artist}
+                  date={song.date}
+                  img={song.img}
+                  rank={song.rank}
+                  title={song.title}
+                  key={index}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious
+            className="absolute translate-x-[100%] hidden group-hover/outer:flex"
+            variant={"secondary"}
+          />
+          <CarouselNext
+            className="absolute -translate-x-[100%] hidden group-hover/outer:flex"
+            variant={"secondary"}
+          />
+        </Carousel>
       </div>
     </div>
   );

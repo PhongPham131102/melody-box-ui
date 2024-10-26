91fe022dda6d939412c3294afabb8aaa0d3f8782
Pham Ngoc Phong <phongpham131102@gmail.com>
2024-10-26 11:34:36 +0700

    ip

--- src/app/(main)/bxh-chart/page.tsx
+++ src/app/(main)/bxh-chart/page.tsx
@@ -1,39 +1,51 @@
 "use client";
 
 import { useAppSelector } from "@/src/lib/hooks/redux.hook";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Stage, Layer, Line } from "react-konva";
 
 export default function BxhChart() {
   const headerHeight = useAppSelector((state) => state.ui.headerHeight);
+  const [canvasSize, setCanvasSize] = useState({ width: 2000, height: 600 });
 
-  useEffect(() => {
-    const chartCanvas = document.getElementById(
-      "chartCanvas"
-    ) as HTMLCanvasElement;
-    if (chartCanvas) {
-      const canvasWidth = chartCanvas.offsetWidth;
-      const canvasHeight = chartCanvas.offsetHeight;
-      const widthPointPerHorizontal = canvasWidth / 13;
-      const widthPointPerVertical = canvasHeight / 5 - 5;
+  // useEffect(() => {
+  //   const chartCanvas = document.getElementById(
+  //     "chartCanvasContainer"
+  //   ) as HTMLDivElement;
+  //   if (chartCanvas) {
+  //     const resizeObserver = new ResizeObserver(() => {
+  //       setCanvasSize({
+  //         width: chartCanvas.offsetWidth * 2,
+  //         height: chartCanvas.offsetHeight * 2,
+  //       });
+  //     });
 
-      const ctx = chartCanvas.getContext("2d");
+  //     resizeObserver.observe(chartCanvas);
 
-      if (ctx) {
-        for (let index = 5; index > 0; index--) {
-          const xPoint = index * widthPointPerVertical;
-          ctx.lineWidth = 0.8;
+  //     return () => {
+  //       resizeObserver.disconnect();
+  //     };
+  //   }
+  // }, []);
 
-          ctx.beginPath();
-          ctx.setLineDash([5, 5]);
-          ctx.moveTo(0, xPoint + 1);
-          ctx.strokeStyle = "red";
-          ctx.lineTo(1180, xPoint + 1);
-          ctx.stroke();
-        }
-        ctx.setLineDash([]);
-      }
-    }
-  }, []);
+  const widthPointPerHorizontal = canvasSize.width / 13;
+  const widthPointPerVertical = (canvasSize.height / 5 - 5) * 0.5;
+
+  const lines = [];
+  for (let index = 5; index > 0; index--) {
+    let yPoint = index * widthPointPerVertical;
+    yPoint += index % 2 === 0 ? 0.5 : 0;
+    console.log(yPoint);
+    lines.push(
+      <Line
+        key={index}
+        points={[0, yPoint + 1, canvasSize.width * 0.5, yPoint + 1]}
+        stroke="red"
+        strokeWidth={0.5}
+        dash={[5, 5]}
+      />
+    );
+  }
 
   return (
     <div
@@ -41,14 +53,11 @@ export default function BxhChart() {
         height:
           headerHeight === 0 ? "0" : `calc(100vh - ${headerHeight + 90}px)`,
       }}
-      className=" relative overflow-hidden select-none flex p-3 w-full"
+      className="relative overflow-hidden select-none flex p-3 w-full"
     >
-      <canvas
-        id="chartCanvas"
-        className="w-[1188px] h-[300px] "
-        width="1188"
-        height="300"
-      ></canvas>
+      <Stage width={canvasSize.width} height={canvasSize.height}>
+        <Layer>{lines}</Layer>
+      </Stage>
     </div>
   );
 }
